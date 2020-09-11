0487104245132a17d0ba0876b0891eefc677ea19
Michael <michaelgatesjr@gmail.com>
2020-09-11 23:56:18 +0100

    added animation to photo-grid

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -4,6 +4,7 @@ import ShopPic2 from "../assets/shop2.png";
 import ShopPic3 from "../assets/shop3.png";
 import ShopPic4 from "../assets/shop4.png";
 import ShopPic5 from "../assets/shop5.png";
+import { motion } from "framer-motion";
 
 const PhotoGrid = () => {
   return (
@@ -11,11 +12,47 @@ const PhotoGrid = () => {
       <section className='photo__grid'>
         <div className='container photo__grid__container'>
           <div className='photo__grid__system'>
-            <img src={ShopPic1} className='pic1' alt='shop_pic' />
-            <img src={ShopPic2} className='pic2' alt='shop_pic' />
-            <img src={ShopPic3} className='pic3' alt='shop_pic' />
-            <img src={ShopPic4} className='pic4' alt='shop_pic' />
-            <img src={ShopPic5} className='pic5' alt='shop_pic' />
+            <motion.img
+              animate={{ opacity: 1, scale: 1 }}
+              initial={{ opacity: 0, scale: 0 }}
+              transition={{ duration: 1.5 }}
+              src={ShopPic1}
+              className='pic1'
+              alt='shop_pic'
+            />
+            <motion.img
+              animate={{ opacity: 1, scale: 1 }}
+              initial={{ opacity: 0, scale: 0 }}
+              transition={{ duration: 2 }}
+              img
+              src={ShopPic2}
+              className='pic2'
+              alt='shop_pic'
+            />
+            <motion.img
+              animate={{ opacity: 1, scale: 1 }}
+              initial={{ opacity: 0, scale: 0 }}
+              transition={{ duration: 2.5 }}
+              src={ShopPic3}
+              className='pic3'
+              alt='shop_pic'
+            />
+            <motion.img
+              animate={{ opacity: 1, scale: 1 }}
+              initial={{ opacity: 0, scale: 0 }}
+              transition={{ duration: 3 }}
+              src={ShopPic4}
+              className='pic4'
+              alt='shop_pic'
+            />
+            <motion.img
+              animate={{ opacity: 1, scale: 1 }}
+              initial={{ opacity: 0, scale: 0 }}
+              transition={{ duration: 2.2 }}
+              src={ShopPic5}
+              className='pic5'
+              alt='shop_pic'
+            />
           </div>
         </div>
       </section>
